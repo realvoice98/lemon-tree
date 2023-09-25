60133032fc324d14fffece82bcda26b6f3052bf8
KSH-99 <kk00011157@gmail.com>
2023-09-25 23:27:33 +0900

    desk_main 파일명 main으로 변경

--- admin/app.js
+++ admin/app.js
@@ -8,7 +8,19 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/static'))
 
 app.get('/', (req, res) => {
-    res.render('desk_main');
+    res.render('main');
+  })
+
+app.get('/reservation_manage', (req, res) => {
+    res.render('reservation_manage');
+  })
+
+app.get('/client_list', (req, res) => {
+    res.render('client_list');
+  })
+
+app.get('/prog_manage', (req, res) => {
+    res.render('prog_manage');
   })
 
 app.listen(port, () => {
