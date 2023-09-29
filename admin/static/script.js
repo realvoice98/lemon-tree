bc8b86611f893efe541dbd8ea366f78d8c799b01
KSH-99 <kk00011157@gmail.com>
2023-09-29 17:24:32 +0900

    모달 기능 추가

--- admin/static/script.js
+++ admin/static/script.js
@@ -1,6 +1,15 @@
 // 모든 "row" 요소를 선택합니다.
 const rows = document.querySelectorAll('.row');
 
+// 모달 요소를 선택합니다.
+const modal = document.querySelector('.modal');
+
+// 백그라운드 요소를 선택합니다.
+const modalBack = document.querySelector('.modal_back');
+
+// 클릭한 로우의 정보를 저장할 객체를 초기화합니다.
+let clickedRowData = null;
+
 // 각 "row" 요소에 클릭 이벤트를 추가합니다.
 rows.forEach(row => {
   row.addEventListener('click', function() {
@@ -30,5 +39,16 @@ rows.forEach(row => {
 
     // 추출한 데이터를 콘솔에 출력합니다.
     console.log('클릭된 행 데이터:', rowData);
+
+    // 클릭된 로우의 데이터를 저장합니다.
+    clickedRowData = rowData;
+
+    // 백그라운드를 보이게 합니다.
+    modalBack.style.display = 'block';
   });
+});
+
+// 백그라운드 클릭 시 모달을 닫습니다.
+modalBack.addEventListener('click', function () {
+  modalBack.style.display = 'none';
 });
