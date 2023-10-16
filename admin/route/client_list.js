a9b4c880840f989b3f1f43c6c80fc807558c0be2
KSH-99 <kk00011157@gmail.com>
2023-10-16 17:09:58 +0900

    페이지 출력

--- admin/route/client_list.js
+++ admin/route/client_list.js
@@ -1,9 +1,17 @@
 const express = require('express');
-
 const router = express.Router();
+const db = require('./db');
 
 router.get('/admin/client_list', (req, res) => {
-    res.render('client_list');
+    db.query('SELECT * FROM client', (error, results, fields) => {
+      if (error) {
+        console.error('쿼리 실행 오류: ' + error);
+        res.status(500).send('서버 오류');
+        return;
+      }
+
+      res.render('client_list', { clients: results });
+    });
   })
 
 module.exports = router;
--- admin/route/reserv_manage.js
+++ admin/route/reserv_manage.js
@@ -1,9 +1,17 @@
 const express = require('express');
-
 const router = express.Router();
+const db = require('./db');
 
 router.get('/admin/reservation_manage', (req, res) => {
-    res.render('reservation_manage');
+    db.query('SELECT * FROM reservations', (error, results, fields) => {
+      if (error) {
+        console.error('쿼리 실행 오류: ' + error);
+        res.status(500).send('서버 오류');
+        return;
+      }
+
+      res.render('reservation_manage', { reservations: results });
+    });
   })
 
 module.exports = router;
