e9f19430ea4830b6f2af63f05977eb098df275ca
KSH-99 <kk00011157@gmail.com>
2023-10-24 21:06:31 +0900

    매출관리 일자별 검색 추가&css 수정

--- admin/static/client_list_script.js
+++ admin/static/client_list_script.js
@@ -1,7 +1,15 @@
+// SVG 아이콘을 클릭할 때 form을 제출하는 함수
+document.getElementById('searchIcon').addEventListener('click', function() {
+  document.getElementById('searchForm').submit();
+});
+document.getElementById('searchIcon2').addEventListener('click', function() {
+  document.getElementById('searchForm2').submit();
+});
+
 function searchOnEnter(event) {
     if (event.key === 'Enter') {
         const searchData = document.querySelector('.searchInput').value;
-        const url = `/admin/client_list/client_search?searchData=${encodeURIComponent(searchData)}`;
+        const url = `/admin/client_list/search?searchData=${encodeURIComponent(searchData)}`;
         window.location.href = url;
         return false; // 폼 제출을 중단
       }
@@ -10,8 +18,10 @@ function searchOnEnter(event) {
 function tablet_searchOnEnter(event) {
     if (event.key === 'Enter') {
         const searchData = document.querySelector('.searchInput').value;
-        const url = `/admin/client_list/client_search?searchData=${encodeURIComponent(searchData)}`;
+        const url = `/admin/client_list/search?searchData=${encodeURIComponent(searchData)}`;
         window.location.href = url;
         return false; // 폼 제출을 중단
       }
 }
+
+
