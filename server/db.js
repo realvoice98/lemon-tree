794c0f02a615257543f3535d90292dfb62fa19ca
KSH-99 <kk00011157@gmail.com>
2023-10-27 00:11:55 +0900

    db호스트 수정

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host : 'lttest.cafe24app.com',
+    host : 'lemontree.cafe24app.com',
     user : 'lt123',
     password : 'lttest123',
     database : 'lt123'
